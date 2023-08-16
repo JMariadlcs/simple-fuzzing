b0b5268eb7e81495c72362ceeb99f70e7d827f78
JMariadlcs <jmariadelacruzsanchez@gmail.com>
2023-08-16 23:35:21 +0200

    add new fuzzing functions

--- test/CloneTest.t.sol
+++ test/CloneTest.t.sol
@@ -8,6 +8,7 @@ import "openzeppelin-contracts/contracts/proxy/Clones.sol";
 
 contract CloneTest is Test {
     
+    // FUZZ DEPLOYER
     function test_fuzz_predictDeterministicAddressDeployer(address deployer) external {
         bytes32 salt = keccak256(abi.encodePacked("the salt"));
         uint256 number = 77;
@@ -24,4 +25,37 @@ contract CloneTest is Test {
 
         assertEq(predictedAddress, cloneAddress);
     }
+
+    // FUZZ DEPLOYER, SALT
+    function test_fuzz_predictDeterministicAddressSalt(address deployer, bytes32 salt) external {
+        uint256 number = 77;
+
+        vm.startPrank(deployer);
+
+        SimpleContract implementation = new SimpleContract(number);
+
+        address cloneAddress = Clones.cloneDeterministic(address(implementation), salt);
+        assertEq(implementation.getNumber(), number);
+        assertEq(SimpleContract(cloneAddress).getNumber(), number);
+
+        address predictedAddress = Clones.predictDeterministicAddress(address(implementation), salt, deployer);
+
+        assertEq(predictedAddress, cloneAddress);
+    }
+
+    // FUZZ DEPLOYER, SALT, NUMBER
+    function test_fuzz_predictDeterministicAddressNumber(address deployer, bytes32 salt, uint256 number) external {
+        vm.startPrank(deployer);
+
+        SimpleContract implementation = new SimpleContract(number);
+
+        address cloneAddress = Clones.cloneDeterministic(address(implementation), salt);
+        assertEq(implementation.getNumber(), number);
+        assertEq(SimpleContract(cloneAddress).getNumber(), number);
+
+        address predictedAddress = Clones.predictDeterministicAddress(address(implementation), salt, deployer);
+
+        assertEq(predictedAddress, cloneAddress);
+    }
+
 }
